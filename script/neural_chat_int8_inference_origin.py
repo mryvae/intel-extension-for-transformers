a65aabc22586af96970f8246698c99aa925922d8
mryvae <1312348150@qq.com>
2023-12-13 06:31:46 +0000

    modify script/neural_chat_int4_inference_origin

--- script/neural_chat_int8_inference_origin.py
+++ script/neural_chat_int8_inference_origin.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, TextStreamer
 from intel_extension_for_transformers.transformers import AutoModelForCausalLM
+import cProfile
 model_name = "Intel/neural-chat-7b-v1-1"     # Hugging Face model_id or local model
 prompt = "Once upon a time, there existed a little girl,"
 
@@ -8,4 +9,8 @@ inputs = tokenizer(prompt, return_tensors="pt").input_ids
 streamer = TextStreamer(tokenizer)
 
 model = AutoModelForCausalLM.from_pretrained(model_name, load_in_8bit=True)
+profiler = cProfile.Profile()
+profiler.enable()
 outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
+profiler.disable()
+profiler.dump_stats("profile_data_int8_runtime.prof")
